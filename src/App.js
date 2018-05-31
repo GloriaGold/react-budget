092afb41a09e1670fc3c924bc41c81f1826baa48
Gloria Schulz <gloria.johnson.a@gmail.com>
2018-05-31 14:14:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Spendings from './components/Spendings'
+import Budgetheader from './components/Budgetheader'
+import Lunchbutton from './components/Lunchbutton'
+
+class App extends Component {
+  render() {
+    return <div className="App" />
+  }
+}
+
+export default App
---
+++ src/components/Budgetheader.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Budgetheader extends Component {
+  render() {
+    return <div />
+  }
+}
